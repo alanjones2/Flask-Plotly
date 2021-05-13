a42332836bccd33587f5f900d695c19cd43bd8fd
alanjones2 <jones.alan@hotmail.co.uk>
2021-05-13 13:13:28 +0200

    Add files via upload
    Added callback directory

---
+++ plotlycallback-gm/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
+app = Flask(__name__)
+
+@app.route('/callback', methods=['POST', 'GET'])
+def cb():
+    return gm(request.args.get('data'))
+   
+@app.route('/')
+def index():
+    return render_template('chartsajax.html',  graphJSON=gm())
+
+def gm(country='United Kingdom'):
+    df = pd.DataFrame(px.data.gapminder())
+
+    fig = px.line(df[df['country']==country], x="year", y="gdpPercap")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
